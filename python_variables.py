c88150ffc957659c30a33e65e14637335b79a0ee
ShalomJabali <139133364+ShalomJabali@users.noreply.github.com>
2024-07-31 11:14:02 +0200

    Update python_variables.py

--- python_variables.py
+++ python_variables.py
@@ -4,9 +4,16 @@
 # this is a fundamental python basics : 
 # there is datatypes, variables and it's syntax 
 
+#the below program scripts prints a coder's number maybe on the list, name and language choice  
+
 coder_number = 444
+print (' number : ', coder_number)
+
 coder_name = 'pythonista'
+print('name :', coder_name)
+
 is_it_python = True
+print('is a python your language choice :', is_it_python)
 
 # consider coder_number as a variable_name and 444 as a variable_value
 # so variables has a variable_name which stores a variable_store
