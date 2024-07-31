d5475f9d7d4a4e252f96ee06d58b3d34eedcfce0
ShalomJabali <139133364+ShalomJabali@users.noreply.github.com>
2024-07-31 09:58:23 +0200

    Add files via upload
    here we explain a python variable which has a variable name that contains the variable value

---
+++ python_variables.py
@@ -0,0 +1,13 @@
+
+
+# python is a great language 
+# this is a fundamental python basics : 
+# there is datatypes, variables and it's syntax 
+
+coder_number = 444
+coder_name = 'pythonista'
+is_it_python = True
+
+# consider coder_number as a variable_name and 444 as a variable_value
+# so variables has a variable_name which stores a variable_store
+
