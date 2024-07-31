6ee0a4b580b92ad9d8670a74fb4e68a7b47292b5
ShalomJabali <139133364+ShalomJabali@users.noreply.github.com>
2024-07-31 11:20:58 +0200

    Add files via upload
    we dive onto python numeric datatypes

---
+++ python_numeric_datatypes.py
@@ -0,0 +1,20 @@
+
+# datatypes are types of variables 
+# let's dive into python's built in datatypes :
+
+int_var = 444
+print('this is an integer :',int_var)
+# above dwells an integer variable
+
+flt_var = 444.444
+print('this is a float variable :',flt_var)
+
+# above dwells a float variable 
+
+cmplx_var = 4 + 4j
+print('this is a complex variable :', cmplx_var)
+
+# above dwells a complex variables 
+# these are numeric  based datatypes 
+
+
